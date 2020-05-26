0ccee280fa5636d728fc4e3c99dd6ce2b96e6343
mihmilicio <37940750+mihmilicio@users.noreply.github.com>
2020-05-25 21:09:40 -0300

    Ex. 2 - lista 1 de vetores

---
+++ 2-vetores/2020-05-25/2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  int arraySize = 3;
+  float soma = 0;
+  float notas[arraySize];
+  float media;
+  float maior[2] = {0, 0};
+  float menor[2] = {0, 10000};
+
+  for (int i = 0; i < arraySize; i++) {
+    printf("Insira a nota do aluno %d: ", i);
+    scanf("%f", &notas[i]);
+    soma += notas[i];
+    if (notas[i] > maior[1]) {
+      maior[0] = (float)i;
+      maior[1] = notas[i];
+    }
+    if (notas[i] < menor[1]) {
+      menor[0] = (float)i;
+      menor[1] = notas[i];
+    }
+  }
+
+  media = (float)soma / (float)arraySize;
+
+  printf("Média: %f\n", media);
+  printf("Maior nota é %.2f do aluno %.0f \n", maior[1], maior[0]);
+  printf("Menor nota é %.2f do aluno %.0f \n", menor[1], menor[0]);
+
+  return 0;
+}
--- 2-vetores/2020-05-25/readme.md
+++ 2-vetores/2020-05-25/readme.md
@@ -1,3 +1,5 @@
 # Lista de exercícios da aula
 
 1. Construa um algoritmo que leia um vetor de 15 posições e calcule a média destes valores. Na sequência, apresente na tela os valores que são iguais ou superiores à média.
+
+2. Faça um algoritmo que leia a nota de 15 alunos, calcule a média de todas as notas e apresente a menor e a maior nota.
